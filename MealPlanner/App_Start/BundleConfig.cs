ce9a9f7fa70e23ca37c7f4c1b455668c9900b19a
Cllyde <cllyde_soldat@yahoo.com>
2016-06-02 08:21:32 -0500

    Removed unnecessary comment in bundleconfig.

--- MealPlanner/App_Start/BundleConfig.cs
+++ MealPlanner/App_Start/BundleConfig.cs
@@ -25,7 +25,6 @@ namespace MealPlanner
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      //"~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
